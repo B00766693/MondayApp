ec265f9bfeeccd9ba1cd2e026086c8aafc2a7eb4
B00766693 <44704607+B00766693@users.noreply.github.com>
2018-12-13 21:34:22 +0000

    Add files via upload

---
+++ carDataForDB.sql
@@ -0,0 +1,44 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1265\cocoasubrtf210
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red70\green137\blue204;\red23\green23\blue23;\red202\green202\blue202;
+\red167\green197\blue152;\red194\green126\blue101;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
+
+\f0\fs24 \cf2 \cb3 create\cf4 \cb3  \cf2 \cb3 table\cf4 \cb3  CAR_DATA (\
+\pard\pardeftab720\sl360
+\cf4 \'a0\'a0\'a0\'a0id \cf2 \cb3 INT\cf4 \cb3 ,\
+\'a0\'a0\'a0\'a0first_name \cf2 \cb3 VARCHAR\cf4 \cb3 (\cf5 \cb3 50\cf4 \cb3 ),\
+\'a0\'a0\'a0\'a0last_name \cf2 \cb3 VARCHAR\cf4 \cb3 (\cf5 \cb3 50\cf4 \cb3 ),\
+\'a0\'a0\'a0\'a0email \cf2 \cb3 VARCHAR\cf4 \cb3 (\cf5 \cb3 50\cf4 \cb3 ),\
+\'a0\'a0\'a0\'a0car_model \cf2 \cb3 VARCHAR\cf4 \cb3 (\cf5 \cb3 50\cf4 \cb3 ),\
+\'a0\'a0\'a0\'a0car_price \cf2 \cb3 VARCHAR\cf4 \cb3 (\cf5 \cb3 50\cf4 \cb3 )\
+);\
+\pard\pardeftab720\sl360
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 1\cf4 \cb3 , \cf6 'Ethelind'\cf4 , \cf6 'Colbeck'\cf4 , \cf6 'ecolbeck0@ycombinator.com'\cf4 , \cf6 'RX-7'\cf4 , \cf6 '\'8026524,65'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 2\cf4 \cb3 , \cf6 'Baldwin'\cf4 , \cf6 'Tasker'\cf4 , \cf6 'btasker1@linkedin.com'\cf4 , \cf6 'Q'\cf4 , \cf6 '\'8029224,07'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 3\cf4 \cb3 , \cf6 'Osgood'\cf4 , \cf6 'Cahen'\cf4 , \cf6 'ocahen2@independent.co.uk'\cf4 , \cf6 'Town Car'\cf4 , \cf6 '\'8021175,91'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 4\cf4 \cb3 , \cf6 'Cariotta'\cf4 , \cf6 'Sumshon'\cf4 , \cf6 'csumshon3@4shared.com'\cf4 , \cf6 'Continental GTC'\cf4 , \cf6 '\'8018876,71'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 5\cf4 \cb3 , \cf6 'Ware'\cf4 , \cf6 'Mellem'\cf4 , \cf6 'wmellem4@simplemachines.org'\cf4 , \cf6 'T100'\cf4 , \cf6 '\'8094239,80'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 6\cf4 \cb3 , \cf6 'Collete'\cf4 , \cf6 'Bartak'\cf4 , \cf6 'cbartak5@taobao.com'\cf4 , \cf6 'Silverado 2500'\cf4 , \cf6 '\'8099786,26'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 7\cf4 \cb3 , \cf6 'Adelaide'\cf4 , \cf6 'Shurrocks'\cf4 , \cf6 'ashurrocks6@mail.ru'\cf4 , \cf6 'Excel'\cf4 , \cf6 '\'8019538,55'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 8\cf4 \cb3 , \cf6 'Aura'\cf4 , \cf6 'Cowley'\cf4 , \cf6 'acowley7@purevolume.com'\cf4 , \cf6 'S6'\cf4 , \cf6 '\'8087607,48'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 9\cf4 \cb3 , \cf6 'Pandora'\cf4 , \cf6 'Larkcum'\cf4 , \cf6 'plarkcum8@tripod.com'\cf4 , \cf6 'XG350'\cf4 , \cf6 '\'8095265,49'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 10\cf4 \cb3 , \cf6 'Norry'\cf4 , \cf6 'Spary'\cf4 , \cf6 'nspary9@youtube.com'\cf4 , \cf6 '9-5'\cf4 , \cf6 '\'8045675,09'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 11\cf4 \cb3 , \cf6 'Timothee'\cf4 , \cf6 'Casoni'\cf4 , \cf6 'tcasonia@issuu.com'\cf4 , \cf6 '400SEL'\cf4 , \cf6 '\'8098041,23'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 12\cf4 \cb3 , \cf6 'Kendra'\cf4 , \cf6 'Yeatman'\cf4 , \cf6 'kyeatmanb@prweb.com'\cf4 , \cf6 'TrailBlazer'\cf4 , \cf6 '\'8091486,29'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 13\cf4 \cb3 , \cf6 'Rowney'\cf4 , \cf6 'Stearn'\cf4 , \cf6 'rstearnc@wiley.com'\cf4 , \cf6 'LaCrosse'\cf4 , \cf6 '\'8027223,90'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 14\cf4 \cb3 , \cf6 'Dominica'\cf4 , \cf6 'Pawlata'\cf4 , \cf6 'dpawlatad@apache.org'\cf4 , \cf6 'GLK-Class'\cf4 , \cf6 '\'8036200,87'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 15\cf4 \cb3 , \cf6 'Ambrosius'\cf4 , \cf6 'Trayling'\cf4 , \cf6 'atraylinge@g.co'\cf4 , \cf6 'Amanti'\cf4 , \cf6 '\'8078185,70'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 16\cf4 \cb3 , \cf6 'Isidro'\cf4 , \cf6 'Suckling'\cf4 , \cf6 'isucklingf@weebly.com'\cf4 , \cf6 'Cherokee'\cf4 , \cf6 '\'8049442,55'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 17\cf4 \cb3 , \cf6 'Colene'\cf4 , \cf6 'Dowers'\cf4 , \cf6 'cdowersg@ebay.co.uk'\cf4 , \cf6 'Express 3500'\cf4 , \cf6 '\'8042321,17'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 18\cf4 \cb3 , \cf6 'Concettina'\cf4 , \cf6 'Irlam'\cf4 , \cf6 'cirlamh@goodreads.com'\cf4 , \cf6 'Navigator L'\cf4 , \cf6 '\'8085489,82'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 19\cf4 \cb3 , \cf6 'Melody'\cf4 , \cf6 'Peetermann'\cf4 , \cf6 'mpeetermanni@quantcast.com'\cf4 , \cf6 'Ranger'\cf4 , \cf6 '\'8090253,84'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 20\cf4 \cb3 , \cf6 'Annis'\cf4 , \cf6 'Tussaine'\cf4 , \cf6 'atussainej@tamu.edu'\cf4 , \cf6 'Esteem'\cf4 , \cf6 '\'8082731,18'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 21\cf4 \cb3 , \cf6 'Sabra'\cf4 , \cf6 'Greim'\cf4 , \cf6 'sgreimk@imgur.com'\cf4 , \cf6 'Viper RT/10'\cf4 , \cf6 '\'8019160,56'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 22\cf4 \cb3 , \cf6 'Erica'\cf4 , \cf6 'Tinker'\cf4 , \cf6 'etinkerl@linkedin.com'\cf4 , \cf6 'Freestar'\cf4 , \cf6 '\'8023813,94'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 23\cf4 \cb3 , \cf6 'Brunhilda'\cf4 , \cf6 'McCallam'\cf4 , \cf6 'bmccallamm@independent.co.uk'\cf4 , \cf6 'GX'\cf4 , \cf6 '\'8014518,49'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 24\cf4 \cb3 , \cf6 'Ab'\cf4 , \cf6 'Whitta'\cf4 , \cf6 'awhittan@free.fr'\cf4 , \cf6 'Grand Prix'\cf4 , \cf6 '\'8015831,78'\cf4 );\
+\cf2 \cb3 insert\cf4 \cb3  \cf2 \cb3 into\cf4 \cb3  CAR_DATA (id, first_name, last_name, email, car_model, car_price) \cf2 \cb3 values\cf4 \cb3  (\cf5 \cb3 25\cf4 \cb3 , \cf6 'Nicki'\cf4 , \cf6 'Fennelow'\cf4 , \cf6 'nfennelowo@topsy.com'\cf4 , \cf6 'Camaro'\cf4 , \cf6 '\'8073906,27'\cf4 );\
+\
+}
